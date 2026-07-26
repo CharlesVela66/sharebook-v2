0fec77f4b29e86b96d4ade1abe9a7c87f2f2c39d
Carlos <carlosed.velasco@gmail.com>
2026-07-25 18:19:08 -0600

    feat: add icon to input

--- components/search/SearchInput.tsx
+++ components/search/SearchInput.tsx
@@ -1,7 +1,13 @@
-import { Input } from "../ui/input";
+import { Search } from "lucide-react";
+import { InputGroup, InputGroupAddon, InputGroupInput } from "../ui/input-group";
 
 export default function SearchInput(){
     return (
-        <Input type="text" placeholder="Search books, authors, ISBN..." className="bg-background/20 font-semibold text-cards placeholder:text-cards/60 w-1/3 px-3 py-4.5 border-0"/>
+        <InputGroup className="w-1/3 bg-background/20 font-semibold px-1 py-4.5 border-0">
+            <InputGroupInput type="text" placeholder="Search books, authors, ISBN..." className="text-cards placeholder:text-cards/60"/>
+            <InputGroupAddon>
+                <Search className="text-cards"/>
+            </InputGroupAddon>
+        </InputGroup>
     )
 }
