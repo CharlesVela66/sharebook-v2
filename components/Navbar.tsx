946d643f9cd98491babbfda47c84d0413bd42db5
Carlos <carlosed.velasco@gmail.com>
2026-07-25 18:12:47 -0600

    feat: finish default navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,16 @@
+import { Bell } from "lucide-react";
 import SearchInput from "./search/SearchInput";
 
 export default function Navbar(){
     return (
         <nav className="w-full bg-secondary flex justify-between py-3 px-8">
-            <div className="flex gap-10">
+            <div className="w-full flex gap-10">
                 <span className="font-bold text-3xl text-primary">sharebook</span>
                 <SearchInput />
             </div>
-            <div>
-                Profile
+            <div className="flex items-center gap-8">
+                <Bell className="text-background w-5 h-5"/>
+                <div className="w-8 h-8 bg-muted rounded-full"/>
             </div>
         </nav>
     )
--- components/search/SearchInput.tsx
+++ components/search/SearchInput.tsx
@@ -1,7 +1,7 @@
+import { Input } from "../ui/input";
+
 export default function SearchInput(){
     return (
-        <div>
-            SearchInput
-        </div>
+        <Input type="text" placeholder="Search books, authors, ISBN..." className="bg-background/20 font-semibold text-cards placeholder:text-cards/60 w-1/3 px-3 py-4.5 border-0"/>
     )
 }
