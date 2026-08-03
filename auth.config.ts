95664c3708ca131c80f1965b3707bb7b6e26495a
Carlos <carlosed.velasco@gmail.com>
2026-08-03 09:07:04 -0600

    feat: added jwt and session callbacks

--- auth.config.ts
+++ auth.config.ts
@@ -1,8 +1,21 @@
-import type { NextAuthConfig } from 'next-auth';
+import type { NextAuthConfig, Session } from 'next-auth';
+import type { JWT } from 'next-auth/jwt';
  
 export const authConfig = {
   pages: {
     signIn: '/login',
   },
-  providers: []
+  providers: [],
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    session({ session, token }: { session: Session; token: JWT }) {
+      session.user.id = token.id;
+      return session;
+    },
+  },
 } satisfies NextAuthConfig;
